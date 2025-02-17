5334ad332355e380c4b90153f3f99c521464307d
Daniil <54244843+pasosh12@users.noreply.github.com>
2025-02-17 18:04:48 +0300

    Update mumbling.js

--- 7-kyu/mumbling/mumbling.js
+++ 7-kyu/mumbling/mumbling.js
@@ -4,11 +4,7 @@ function accum(s) {
             acc = ''
             for (let i = 0; i < chars.length; i++) {
                 acc += chars[i].toUpperCase()
-                // let i=0
-                // while i<index
                 for (let j = 0; j < i; j++) {
-​
-                    // console.log(chars[i])
                     acc += chars[i]
                 }
                 if (i !== chars.length - 1) acc += '-'
