ec0ddd4e161d69e18c07cd030d96b7eec01ad417
Daniil <54244843+pasosh12@users.noreply.github.com>
2025-02-20 23:25:40 +0300

    Add solution - CodeHub

---
+++ 7-kyu/currying-functions-multiply-all-elements-in-an-array/currying-functions-multiply-all-elements-in-an-array.js
@@ -0,0 +1,7 @@
+function multiplyAll(arr) {
+            return function (multiplier) {
+                return arr.map(el =>
+                    el * multiplier
+                )
+            }
+        }
