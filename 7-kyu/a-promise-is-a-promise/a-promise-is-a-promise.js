dfe4d02f98151c8376219b8fc786524e74a29bc3
Daniil <54244843+pasosh12@users.noreply.github.com>
2025-03-11 11:06:03 +0300

    Add solution - CodeHub

---
+++ 7-kyu/a-promise-is-a-promise/a-promise-is-a-promise.js
@@ -0,0 +1,7 @@
+function promiseHelloWorld() {
+  // replace this nonsense with your Promise
+  let promise = new Promise((res,rej)=>
+                           res('Hello World!')) 
+  return promise
+​
+}
